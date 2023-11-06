94abce69494b8035a2771dad1a3c5b14d45705da
alenahalm <75882124+alenahalm@users.noreply.github.com>
2023-11-06 15:04:51 +0800

    commentary added

--- LIAN.py
+++ LIAN.py
@@ -1,109 +1,20 @@
-from point import Point
-import math
 import cv2 as cv
-import matplotlib.pyplot as plt
+import math
+from point import Cell, Node
 
-def angle_between_vectors(a1, a2, b1, b2):
-    ax = a2.x - a1.x
-    ay = a2.y - a1.y 
-    bx = b2.x - b1.x
-    by = b2.y - b1.y
-    ab = ax*bx + ay*by
-    a = a1.dist_to(a2)
-    b = b1.dist_to(b2)
-    if a*b == 0:
-        return 360
-    else:
-        cos = round(ab / (a * b), 5)
-        return math.acos(cos)*180/math.pi
-
-def line_of_sight(image, a, b):
-
-    dx = b.x - a.x
-    dy = b.y - a.y
-
-    d = dy - (dx/2)  
-    x = a.x 
-    y = a.y  
-
+# midpoint
+def circle_successors(a: Node, r: int):
     points = []
-
-    points.append(Point(x, y))
-    
-    while (x < b.x): 
-        x=x+1
-        
-        if(d < 0): 
-            d = d + dy  
-
-        else: 
-            d = d + (dy - dx)  
-            y=y+1
-
-        points.append(Point(x, y))
-    
-    for i in points:
-        if not i.is_empty(image):
-            return False
-
-    return True
-
-def expand(image, a, delta, goal, a_m, bp, g, CLOSED):
-
-    points = midpoint(a, delta)
-    
-    if a.dist_to(goal) < delta and goal not in points:
-        points.append(goal)
-    open = []
-    ret = True
-    bp_changed = False    
-    for point in points:
-        k = (point.x, point.y)
-        if not point.is_empty(image):
-            CLOSED.append(a)
-            ret = False
-            continue
-        if bp[(a.x, a.y)] != None and angle_between_vectors(bp[(a.x, a.y)], a, a, point) > a_m:
-            continue
-        for close_point in CLOSED:
-            if point == close_point and bp[k] == close_point:
-                continue
-        if not line_of_sight(image, a, point):
-            continue
-
-        if not (k in bp and bp[k] is not None):
-            g[k] = g[(a.x, a.y)] + a.dist_to(point)
-            bp[k] = a
-            bp_changed = True
-            # if k == (89, 129):
-            #     print('new 89, 129:', a)
-            open.append(point)
-        else:
-            if g[k] > g[(a.x, a.y)] + a.dist_to(point):
-                # if k == (89, 129):
-                #     print('changed 89, 129:', a)
-                #     print('prev', bp[k])
-                g[k] = g[(a.x, a.y)] + a.dist_to(point)
-                bp[k] = a
-                bp_changed = True
-
-                open.append(point)
-    # if bp_changed:
-    #     print('changed', bp[k])
-    return ret, open
-
-def midpoint(point: Point, r: int):
-    points = []
-    x_centre = point.x
-    y_centre = point.y
+    x_centre = a.cell.x
+    y_centre = a.cell.y
 
     x = r
     y = 0
-    points.append(Point(x + x_centre, y + y_centre))
+    points.append(Cell(x + x_centre, y + y_centre))
     if r > 0:
-        points.append(Point(-x + x_centre, -y + y_centre))
-        points.append(Point(y + x_centre, x + y_centre))
-        points.append(Point(-y + x_centre, -x + y_centre))
+        points.append(Cell(-x + x_centre, -y + y_centre))
+        points.append(Cell(y + x_centre, x + y_centre))
+        points.append(Cell(-y + x_centre, -x + y_centre))
 	
     P = 1 - r 
 
@@ -119,99 +30,188 @@ def midpoint(point: Point, r: int):
         if (x < y):
             break
 		
-        points.append(Point(x + x_centre, y + y_centre))
-        points.append(Point(-x + x_centre, y + y_centre))
-        points.append(Point(x + x_centre, -y + y_centre))
-        points.append(Point(-x + x_centre, -y + y_centre))
+        points.append(Cell(x + x_centre, y + y_centre))
+        points.append(Cell(-x + x_centre, y + y_centre))
+        points.append(Cell(x + x_centre, -y + y_centre))
+        points.append(Cell(-x + x_centre, -y + y_centre))
 		
         if x != y:
-            points.append(Point(y + x_centre, x + y_centre))
-            points.append(Point(-y + x_centre, x + y_centre))
-            points.append(Point(y + x_centre, -x + y_centre))
-            points.append(Point(-y + x_centre, -x + y_centre))
+            points.append(Cell(y + x_centre, x + y_centre))
+            points.append(Cell(-y + x_centre, x + y_centre))
+            points.append(Cell(y + x_centre, -x + y_centre))
+            points.append(Cell(-y + x_centre, -x + y_centre))
+
+    for i in range(len(points)):
+        points[i] = Node(points[i], a.g + a.cell.dist(points[i]), a)
 
     return points
 
-def clear_bp(bp):
-    return 
+# проверка угла
+def angle_between_vectors(a1:Cell, a2:Cell, b1:Cell, b2:Cell) -> float:
+    ax = a2.x - a1.x
+    ay = a2.y - a1.y 
+    bx = b2.x - b1.x
+    by = b2.y - b1.y
+    ab = ax * bx + ay * by
+    a = a1.dist(a2)
+    b = b1.dist(b2)
+    if a * b == 0:
+        return 360
+    else:
+        cos = round(ab / (a * b), 5)
+        return math.acos(cos) * 180 / math.pi
 
-def LIAN(start, end, delta, a_m, image):
+# есть ли препятствие
+def line_of_sight(image, a: Cell, b: Cell) -> bool:
 
-    OPEN = []
-    CLOSE = []
+    dx = b.x - a.x
+    dy = b.y - a.y
+
+    d = dy - (dx/2)  
+    x = a.x 
+    y = a.y
+
+    line = Cell(x, y)
+
+    if not line.is_traversable(image):
+        return False
+    
+    while (x < b.x): 
+        x = x + 1
+        
+        if(d < 0): 
+            d = d + dy  
+
+        else: 
+            d = d + (dy - dx)  
+            y = y + 1
+
+        line.x = x
+        line.y = y
+        if not line.is_traversable(image):
+            return False
+
+    return True
+
+def expand(image, a: Node, goal: Cell, delta: int, a_m: int, bp: dict, g: dict, CLOSED: list):
+
+    # list с результатом 
+    open = []
+
+    # находим окружность
+    SUCC = circle_successors(a, delta)
+
+    # если цель ближе, чем delta, то добавляем в потенциальные
+    if a.cell.dist(goal) < delta:
+        SUCC.append(Node(goal, a.g + a.cell.dist(goal), a))
+    
+    for s in SUCC: 
+        assert isinstance(s, Node) # все элементы SUCC должны быть Node
+
+        # не рассматриваем точки с припятствием
+        if not s.cell.is_traversable(image):
+            continue
+        
+        # если у [a] в пути есть родитель, проверяем угол поворота.
+        if bp[a.cell.index()] is not None and angle_between_vectors(bp[a.cell.index()].cell, a.cell, a.cell, s.cell) > a_m:
+            continue
+
+        # проходимся по рассмотренным точкам и сравниваем с потенциальной
+        was_checked = False
+        for cp in CLOSED:
+            assert isinstance(cp, Node)
+            # если их координаты совпадают
+            # и координаты родителей тоже
+            # отбрасываем точку
+            if s.cell == cp.cell and s.parent.cell == cp.parent.cell:
+                was_checked = True
+                break
+        if was_checked:
+            continue
+
+        # проверка на препятствие между точками
+        if not line_of_sight(image, a.cell, s.cell):
+            continue
+        
+        # если такой точки в словарях нет
+        # или новое значение лучше существующего 
+        # изменяем словари
+        # и добавляем в OPEN
+        if (s.cell.index() in g and g[s.cell.index()] > s.g) or s.cell.index() not in g:
+            g[s.cell.index()] = s.g
+            bp[s.cell.index()] = s.parent
+            open.append(s)
+
+    return open
+
+
+def LIAN(image, start: Cell, goal: Cell, delta: int, a_m: int):
+
+    # bp - это словарь. Ключ - координаты пикселя, значение - Node родителя
     bp = {}
-    bp[(start.x, start.y)] = None
+    bp[start.index()] = None
     g = {}
-    g[(start.x, start.y)] = 0
+    g[start.index()] = 0
 
+    # стартовая Node.
+    st = Node(start, g[start.index()], None)
 
-    OPEN.append(start)
-    i = 0
+    OPEN = [st]
+    CLOSED = []
+    c = 0
     while len(OPEN) > 0:
-        a = sorted(OPEN, key=lambda x: x.priority + g[(x.x, x.y)])[0]
+        # Сортируем OPEN по сумме расстояния до старта (g) и эвристического расстояния до цели
+        # и берем нулевой элемент
+        a = sorted(OPEN, key=lambda x: x.g + x.cell.dist(goal))[0]
         OPEN.remove(a)
-        print(f'iteration {i}')
-        i+=1
-        print('new a')
-        if a == end:
-            print("Path found!")
+
+        if a == goal:
+            # восстанавливаем путь
             path = []
-            cur_point = end
-            while cur_point != start:
-                print(cur_point, '<', bp[(cur_point.x, cur_point.y)])
-                path.append(cur_point)
-                cur_point = bp[(cur_point.x, cur_point.y)]
-            path.append(start)
+            cur_node = a.cell
+            while cur_node != start:
+                path.append(cur_node)
+                cur_node = bp[cur_node.index()].cell
             path.reverse()
-            return path, bp, g
-        
-        CLOSE.append(a)
+            print("Path found!")
+            return path
+        # добавляем a в рассмотренные
+        if a not in CLOSED:
+            CLOSED.append(a)
 
+        # вызываем expand и получаем list потенциальных Node
+        exp = expand(image, a, goal, delta, a_m, bp, g, CLOSED)
 
+        # дополняем OPEN
+        for n in exp:
+            if n not in OPEN:
+                OPEN.append(n)
 
-        ret, exp = expand(image, a, delta, end, a_m, bp, g, CLOSE)
-        print('received from expand')
+        # Отображение процесса. Черный - текущий путь. Серый - все рассмотренные точки. Зеленый - рассматриваемые точки.
+        copy = image.copy()
+        copy = cv.cvtColor(copy, cv.COLOR_GRAY2BGR)
+        copy = cv.putText(copy, f'{c}', (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv.LINE_AA)
 
-        # if not ret:
-            # continue
+        for p in CLOSED:
+            copy = cv.circle(copy, p.cell.index(), 1, (200, 200, 200), -1)
 
+        for p in OPEN:
+            copy = cv.circle(copy, p.cell.index(), 1, (0, 125, 0), -1)
 
-        # if not ret:
-            # current_point = a
-            # keys = list(bp.keys())
-            # for k in keys:
-            #     if bp[k] is None:
-            #         continue
-                # else:
-                #     copy = cv.circle(copy, (bp[k].x, bp[k].y), 1, (50, 50, 50), -1)
-                # if (bp[k].x, bp[k].y) != (start.x, start.y) and k != (start.x, start.y):
-                    # copy = cv.circle(image, k, 1, (50, 50, 50), -1)
-                    # plt.imshow(copy)
-                    # plt.show()
-                    # CLOSE.append(bp[k])
-                    # if bp[k] in OPEN:
-                    #     OPEN.remove(bp[k])
-                    # bp[k] = None
-            # while current_point != start:
-            #     current_point = bp[(current_point.x, current_point.y)]
-            #     CLOSE.append(bp[(current_point.x, current_point.y)])
-            #     if bp[(current_point.x, current_point.y)] in OPEN:
-            #         OPEN.remove(bp[(current_point.x, current_point.y)])
-            # continue
-        # else:
-        for point in exp:
-            if point not in OPEN:
-                OPEN.append(point)
-        # copy = image.copy()
-        # for p in OPEN:
-        #     copy = cv.circle(copy, (p.x, p.y), 1, (50, 50, 50), -1)
-        # for p in CLOSE:
-        #     copy = cv.circle(copy, (p.x, p.y), 1, (150, 150, 150), -1)
-        # copy = cv.circle(copy, (a.x, a.y), 3, (200, 200, 200), -1)
-        
-        # plt.imshow(copy)
-        # plt.get_current_fig_manager().full_screen_toggle()
-        # plt.show()
-
-    print("Path not found :(")
-    return [], bp, g
+        path = []
+        cur_node = CLOSED[-1].cell
+        while cur_node != start:
+            path.append(cur_node)
+            cur_node = bp[cur_node.index()].cell
+        path.reverse()
+        for p in path:
+            assert isinstance(p, Cell)
+            copy = cv.circle(copy, p.index(), 1, (50, 50, 50), -1)
+        cv.imshow("Window", copy)
+        k = cv.waitKey(1)
+        if k > 0:
+            if chr(k) == 'd':
+                break
+        c += 1
+    print("Path not found")
+    return []
